9fc6fa07cd9c3f2028c1afa018e0b3d50fc11930
Dmytro Yantsybaiev <d.yantsybaiev@icloud.com>
2021-06-16 15:37:41 +0300

    Added the feature of logging in existence users

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
@@ -14,6 +15,16 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
 
     @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+                guard let strongSelf = self else { return }
+                if let e = error {
+                    print(e)
+                } else {
+                    strongSelf.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+            }
+        }
     }
     
 }
